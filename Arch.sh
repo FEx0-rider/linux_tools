3e7bc72fcf6c45676fb0716f18c4326997164577
FEx0 <zdenek.sadilek9@gmail.com>
2025-03-02 22:10:18 +0100

    Create Arch.sh

---
+++ Arch.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Aktualizace repozitářů
+echo "Updating..."
+sudo pacman -Syu --noconfirm
+
+echo
+# Instalace nástrojů
+echo "Installing..."
+echo
+
+# Instalace nástrojů z oficiálních repozitářů
+sudo pacman -S --noconfirm aircrack-ng
+sudo pacman -S --noconfirm sqlmap
+sudo pacman -S --noconfirm nmap
+sudo pacman -S --noconfirm metasploit
+sudo pacman -S --noconfirm hydra
+sudo pacman -S --noconfirm john
+sudo pacman -S --noconfirm nikto
+sudo pacman -S --noconfirm wireshark-gtk  # nebo wireshark-qt
+sudo pacman -S --noconfirm burpsuite
+sudo pacman -S --noconfirm ettercap-gtk
+sudo pacman -S --noconfirm gnu-netcat
+sudo pacman -S --noconfirm gobuster
+sudo pacman -S --noconfirm wifite
+sudo pacman -S --noconfirm airgeddon
+sudo pacman -S --noconfirm dirbuster
+sudo pacman -S --noconfirm exiftool
+sudo pacman -S --noconfirm git  # Přidáno, pokud nemáte nainstalováno git
+
+# Instalace nástrojů z AUR (pokud je potřeba, nainstalujte yay nebo jiný AUR helper)
+if ! command -v yay &> /dev/null; then
+    echo "yay není nainstalován. Instalace yay..."
+    sudo pacman -S yay --noconfirm
+fi
+
+yay -S --noconfirm psudohash
+yay -S --noconfirm whoami-project
+
+echo
+echo "Installed"
